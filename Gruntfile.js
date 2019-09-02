04a63b5af032f9e0602e3a1ffefded727b446fea
Annie Isa'ako <270020786@bun2-206-22.acg.ac.nz>
2019-09-03 11:13:00 +1200

    installed grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,26 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
+    grunt.initConfig({
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'public/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'css/',
+                    ext: '.min.css'
+                }]
+            }
+        },
+        uglify: {
+            my_target: {
+                files: {
+                    'public/js/main.min.js': ['public/js/main.js']
+                }
+            }
+        }
+    });
 
-	});
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify-es');
+
 };
