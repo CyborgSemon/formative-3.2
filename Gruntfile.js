7ea4c446df9e8a5532ff0bc5aff57d9baeb1d0d6
Simon <watsonsemon@gmail.com>
2019-09-03 11:12:54 +1200

    grunt step one set up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,37 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
-
+		sass: {
+			dist: {
+				files: {
+					'public/css/main.css': 'public/scss/main.scss'
+				}
+			}
+		},
+		jshint: {
+			files: ['*.js', '!Gruntfile.js', 'public/js/*.js'],
+			options: {
+				'esversion': 6,
+			}
+		},
+		watch: {
+			sass: {
+				files: ['public/scss/main.scss'],
+				tasks: ['sass', 'cssmin']
+			},
+			frontJs: {
+				files: ['public/js/main.js'],
+				tasks: ['jshint', 'uglify']
+			},
+			backJS: {
+				files: ['server.js'],
+				tasks: ['jshint']
+			}
+		}
 	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['watch'])
 };
